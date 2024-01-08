d46d3532abd5c3c8929d5c4bb437681da5b5f9d2
Alan <a22311050@uthermosillo.edu.mx>
2024-01-08 08:33:31 -0700

    Se preparo el proyecto de AndroidStudio

---
+++ app/src/main/java/mx/edu/uthermosillo/apps/alanortiz/proyectoiot/ExtraFragment.kt
@@ -0,0 +1,19 @@
+package mx.edu.uthermosillo.apps.alanortiz.proyectoiot
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class ExtraFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_extra, container, false)
+        return view
+    }
+}
